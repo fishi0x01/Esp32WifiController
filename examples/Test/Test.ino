6949c54d8f90adf9f1236244c39a4eec2f20c550
Karl Fischer <fishi0x01@gmail.com>
2020-10-28 00:28:56 +0100

    remove serial init

--- examples/Test/Test.ino
+++ examples/Test/Test.ino
@@ -5,10 +5,6 @@ Esp32WifiController wifiController;
 const int PUSHBUTTON_PIN = 15;
 char* BLUETOOTH_NAME = "MyESP32";
 
-void setup()
-{
-    Serial.begin(115200);
-    wifiController.begin(BLUETOOTH_NAME, PUSHBUTTON_PIN);
-}
+void setup() { wifiController.begin(BLUETOOTH_NAME, PUSHBUTTON_PIN); }
 
 void loop() {}
